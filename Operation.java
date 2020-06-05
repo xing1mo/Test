eba0defa2cb6544b9c7797858661f2607bbd8575
xingmo <282696749@qq.com>
2020-06-05 11:34:19 +0800

    add new files

---
+++ Operation.java
@@ -0,0 +1,11 @@
+public class Operation {
+    public static int add(int a,int b)
+    {
+        return a + b;
+    }
+    public static int sub(int a,int b)
+    {
+        return a-b;
+    }
+
+}
